ba54046013eb79f028bdf568994f6f0412d29762
Patricio <patriciohidalgosanchez@gmail.com>
2019-03-22 20:03:22 +0100

    Camera Limits, Tool autoset Node Neighbours, Truck Auto Set first Node

---
+++ HackForGood/Assets/InefficientDirAssignation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class InefficientDirAssignation : MonoBehaviour
+{
+    [MenuItem("Window/AsignarVecinos")]
+    static void AssignNeigbours()
+    {
+
+        Node[] nodes = FindObjectsOfType<Node>();
+
+        foreach (Node node in nodes)
+        {
+            foreach (Node n in nodes)
+            {
+                if (node != n)
+                {
+                    Vector3 dif = (node.transform.position - n.transform.position);
+                    if (1 < dif.magnitude && dif.magnitude < 3)
+                    {
+                        Vector3 ndif = dif.normalized;
+                        if (ndif == Vector3.forward)
+                        {
+                            node.SetNodeAt(n, Directions.Up);
+                            n.SetNodeAt(node, Directions.Down);
+                        }
+                        else if (ndif == Vector3.back)
+                        {
+                            node.SetNodeAt(n, Directions.Down);
+                            n.SetNodeAt(node, Directions.Up);
+                        }
+                        else if (ndif == Vector3.left)
+                        {
+                            node.SetNodeAt(n, Directions.Left);
+                            n.SetNodeAt(node, Directions.Right);
+                        }
+                        else if (ndif == Vector3.right)
+                        {
+                            node.SetNodeAt(n, Directions.Right);
+                            n.SetNodeAt(node, Directions.Left);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- HackForGood/Assets/Scripts/Truck.cs
+++ HackForGood/Assets/Scripts/Truck.cs
@@ -20,8 +20,8 @@ public class Truck : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentDirection = Directions.Right;
-        nextDirection = Directions.Right;
+        SetInitialDirection();
+        destination = GetClosestNode();
     }
 
     // Update is called once per frame
@@ -61,8 +61,8 @@ public class Truck : MonoBehaviour
         transform.LookAt(nextNode.transform);
         destination = destination.GetNodeAtDirection(nextDirection);
 
-        //prueba
-        nextDirection = Directions.Right;
+        ////prueba
+        //nextDirection = Directions.Right;
     }
 
     private void PickUpGarbage()
@@ -73,5 +73,63 @@ public class Truck : MonoBehaviour
             if(g.garbageType == truckType)
                 destination.RemoveGarbage();
         }
+    }
+
+    private Node GetClosestNode()
+    {
+        Node[] nodes = FindObjectsOfType<Node>();
+        Node closestNode = nodes[0];
+        float minDistance = (transform.position - closestNode.transform.position).sqrMagnitude;
+
+        for (int i = 1; i < nodes.Length; i++)
+        {
+            float dist = (nodes[i].transform.position - transform.position).sqrMagnitude;
+            if (dist < minDistance)
+            {
+                minDistance = dist;
+                closestNode = nodes[i];
+            }
+        }
+
+        return closestNode;
+    }
+
+    private void SetInitialDirection()
+    {
+        Directions dir = Directions.Right;
+        Vector3 front = Vector3.Cross(transform.up, transform.forward).normalized;
+        float minAngle = 359;
+        float angle;
+
+        angle = Mathf.Abs(Vector3.Angle(front, Vector3.forward));
+        if (angle < minAngle)
+        {
+            minAngle = angle;
+            dir = Directions.Right;
+        }
+
+        angle = Mathf.Abs(Vector3.Angle(front, Vector3.back));
+        if (angle < minAngle)
+        {
+            minAngle = angle;
+            dir = Directions.Left;
+        }
+
+        angle = Mathf.Abs(Vector3.Angle(front, Vector3.left));
+        if (angle < minAngle)
+        {
+            minAngle = angle;
+            dir = Directions.Down;
+        }
+
+        angle = Mathf.Abs(Vector3.Angle(front, Vector3.right));
+        if (angle < minAngle)
+        {
+            minAngle = angle;
+            dir = Directions.Up;
+        }
+
+        currentDirection = dir;
+        nextDirection = dir;
     }
 }
