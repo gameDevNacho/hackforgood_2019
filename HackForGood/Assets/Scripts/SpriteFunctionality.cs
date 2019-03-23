6538796a1ece6c049eab137e55f1e977f297ff8b
Fernando1702 <45012262+Fernando1702@users.noreply.github.com>
2019-03-23 02:47:30 +0100

    Final

---
+++ HackForGood/Assets/Scripts/SpriteFunctionality.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFunctionality : MonoBehaviour
+{
+  float lifetime = 3;
+  public float currentLifeTime = 0;
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1 - currentLifeTime / lifetime);
+
+    if (currentLifeTime >= lifetime)
+    {
+      Destroy(gameObject);
+    }
+
+    transform.Translate(Vector3.up * 2 * Time.deltaTime,Space.World);
+
+    transform.LookAt(Camera.main.transform);
+
+    currentLifeTime += Time.deltaTime;
+  }
+}
---
+++ HackForGood/Assets/Scripts/SpriteInstancer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteInstancer : MonoBehaviour
+{
+  public Sprite good, bad;
+
+  public static SpriteInstancer Instance
+  {
+    get;
+    private set;
+  }
+
+  // Start is called before the first frame update
+  void Awake()
+  {
+    if (Instance == null)
+    {
+      Instance = this;
+    }
+    else
+    {
+      Destroy(gameObject);
+    }
+
+    //InvokeRepeating("Trial", 0, 2);
+  }
+
+  void Trial()
+  {
+    InstantiateAt(transform.position, false);
+  }
+
+  public void Update()
+  {
+   
+  }
+
+  public void InstantiateAt(Vector3 position, bool good)
+  {
+    GameObject sprite = new GameObject();
+    sprite.transform.position = position;
+    SpriteRenderer renderer = sprite.AddComponent<SpriteRenderer>();
+    renderer.sprite = (good) ? this.good : bad;
+    sprite.AddComponent<SpriteFunctionality>();
+  }
+
+}
